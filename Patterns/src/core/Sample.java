4db666324aae70d01c06c2c25ff594ec903ddfaa
ajinkyab-cuelogic <ajinkya.bhagat@cuelogic.co.in>
2015-03-10 20:09:37 +0530

    added changes

---
+++ Patterns/src/core/Sample.java
@@ -0,0 +1,132 @@
+package core;
+
+import java.util.Scanner;
+
+public class Sample 
+{
+	
+	
+	@SuppressWarnings("resource")
+	public static void main(String args[])
+	{
+		String str = "per month";
+		
+
+		/*int row,i,j,k;
+		System.out.println("Enter any no");
+		Scanner sc=new Scanner(System.in);
+		row=sc.nextInt();
+		*///System.out.println();
+		/*for(i=1;i<=row;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println("");
+		}
+		
+		System.out.println("------------------");
+		
+		for(i=1;i<=row;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+		System.out.println("------------------");
+		
+		for(i=row;i>=1;i--)
+		{
+			for(j=i;j>=1;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+		System.out.println("------------------");
+		
+		for(i=1;i<=row;i++)
+		{
+			for(j=1;j<i;j++)
+			{
+				System.out.print(" ");
+			}
+			
+			for(j=row;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+			
+		System.out.println("------------------");
+		
+		for(i=1;i<=row;i++)
+		{
+			for(j=1;j<=row;j++)
+			{
+				if(i==j)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(j);
+				}
+			}
+			System.out.println("");
+		}
+		
+		System.out.println("------------------");
+		
+		char ch='A'; 
+		for(i=1,ch='A';i<=row;i++,ch++)
+		{	
+			for(j=1;j<=i;j++)
+			{
+				//System.out.print(i);
+				System.out.print(ch);
+				
+			}
+			System.out.println("");
+		}
+		
+		
+		System.out.println("------------------");
+		
+		for(i=1,k=0;i<=row;i++,k++)
+		{
+			for(j=row;j>i;j--)
+			{
+				System.out.print(" ");
+			}
+			
+			for(j=1;j<=(i+k);j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println("");	
+			
+		}
+		
+		
+		System.out.println("------------------");
+		
+		for(i=1;i<=row;i++)
+		{
+			for(j=0;j<i;j++)
+			{
+				System.out.print(());
+			}
+			System.out.println("");
+		}
+		*/
+	}
+	
+}
